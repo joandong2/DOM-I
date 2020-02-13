b11e6354cea1d5287afc7a7dc760825dec61db45
John Oblenda <joandong2@gmail.com>
2020-02-12 22:10:07 -0500

    done stretch project

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,25 +1,52 @@
 let firstITR = 0;
 let secondITR = 0;
-let limit = 30;
+let limit = 10;
 let divisible = 1;
 
-//document.getElementById('msHundreds').textContent = 0;
-//20
+const startButton = document.getElementById('start');
+const resetButton = document.getElementById('reset');
+const digits = document.querySelectorAll('.digits');
 
-const interval = setInterval(function foo() {
-    
-    document.getElementById('msTens').textContent = firstITR;
-    document.getElementById('msHundreds').textContent = secondITR;
-    firstITR++;
+resetButton.onclick = () => {
 
-    if (secondITR*10 === limit) {
-        clearInterval(interval);
-    }
+    document.getElementById('secondTens').textContent = 0;
+    document.getElementById('secondOnes').textContent = 0;
+    document.getElementById('msHundreds').textContent = 0;
+    document.getElementById('msTens').textContent = 0;
+    firstITR = 0;
+    secondITR = 0;
+    divisible = 1;
+
+    startButton.disabled = false;
+
+    digits.forEach((item) => {
+        item.style.color = 'black';
+    });
+}
+
+startButton.onclick = () => {
+
+    startButton.disabled = true;
+
+    const interval = setInterval(() => {
+        
+        document.getElementById('msTens').textContent = firstITR;
+        document.getElementById('msHundreds').textContent = secondITR;
+        
+        firstITR++;
     
-    if (firstITR/10 == 1) {
-        firstITR = 0;
-        secondITR = divisible;
-        divisible++;
-    }
-    
-}, 1000);
+        if (secondITR*10 === limit) {
+            clearInterval(interval);
+            digits.forEach((item) => {
+                item.style.color = 'red';
+            });
+        }
+        
+        if (firstITR/10 == 1) {
+            firstITR = 0;
+            secondITR = divisible;
+            divisible++;
+        }
+        
+    }, 1000);
+}
